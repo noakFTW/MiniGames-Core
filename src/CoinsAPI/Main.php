5752540744a937e8cc6fc9911e4bde09d2f3c3ff
noakFTW <noakFTW@gmail.com>
2016-09-03 13:13:57 -0500

    Update Main.php

--- src/CoinsAPI/Main.php
+++ src/CoinsAPI/Main.php
@@ -1,2 +1,19 @@
 <?php
-//code soon
+
+namespace CoinsAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+
+class CoinsAPI extends PluginBase implements Listener {
+	
+	public function onEnable()
